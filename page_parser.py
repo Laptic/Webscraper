42599b7412d57f9d3d088a0ca6d8d2023daceeca
John <johnerymartinez@gmail.com>
2019-05-24 12:22:54 -0400

    2.Added two more parameters to newEggScraper (filename,writeMode)

--- page_parser.py
+++ page_parser.py
@@ -1,5 +1,6 @@
 import requests
 import lxml.html
+import os
 
 class PageIter:
 #scapper for amazon using https://www.amazon.com/s?k=RAM&i=electronics&qid=1558543022&ref=sr_pg_1
@@ -11,26 +12,33 @@ class PageIter:
 #doesnt get the first three items from the first page and does not scraper the first item from every page after
 #every scrape includes the first item of the next page as the last item of the current page
 #fix above later
+#takes a url and a filename (both strings)
     @staticmethod
-    def newEggScaper(url):
+    def newEggScaper(url,filename,writeMode):
 
         html = requests.get(url,timeout = 5)
         doc = lxml.html.fromstring(html.content)
 
+
         item_blocks = doc.xpath('.//div[@class="item-container   "]')
 
-        items = []
         #writes all ram values to a text file
-        file = open("ram_cards.txt","w+")
-
+        file = open(filename,writeMode)
 
         for item in item_blocks:
 
-            items.append(item.xpath('.//a[@class="item-title"]')[0].text_content())
-            file.write(item.xpath('.//a[@class="item-title"]')[0].text_content() + "\r\n")
+            file.write(item.xpath('.//a[@class="item-title"]')[0].text_content() + ",")
+            file.write(item.xpath('.//li[@class="price-current"]')[0].text_content().replace('\r\n',' ').replace('\t', ' ').replace('|', ' ').split()[0] +"\r\n")
+
 
         file.close()
         return 0
+
+    @staticmethod
+    def newEggPageScraper(url,filename,numOfPages):
+
+        return 0
+
 
 if __name__ == "__main__":
     #print("hello")
@@ -40,5 +48,5 @@ if __name__ == "__main__":
     #block.xpath('.//a[@class="item-title"]')[0].text_content()
     #item_names = new_releases.xpath('.//a[@class="item-title"]')
     #price_items = html_block.xpath('.//li[@class="price-current"]'/text())
-    stuff = PageIter.newEggScaper("https://www.newegg.com/p/pl?Submit=ENE&N=-1&IsNodeId=1&d=ram&page=2&bop=And&PageSize=36&order=BESTMATCH")
+    stuff = PageIter.newEggScaper("https://www.newegg.com/p/pl?d=ram&N=-1&isNodeId=1&Submit=ENE&DEPA=0&Order=BESTMATCH","ram.txt","w+")
     #price.text_content().replace('\r\n',' ').replace('\t', ' ').replace('|', ' ').split()[0]
